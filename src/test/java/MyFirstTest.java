f322d4e2e3f903c1019fe9bddf3212cceea3fccf
Oxyq <seo-x@mail.ru>
2023-02-08 13:03:42 +0300

    Added wrong setValue for 3input

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -26,9 +26,16 @@ public class MyFirstTest {
         $("#userEmail").setValue("email@email.com");
         // $(byText("Male")).click(); // выбор пола
         $x("//label[@for='gender-radio-1']").click(); // выбор пола xpath
-        $("#userNumber").setValue("9999999999");
+        $("#userNumber").setValue("77777777777");
+        //set dateOfBirth 10 Nov 1986
+        $("#dateOfBirthInput").click();
+        $("select[class='react-datepicker__year-select']").$("option[value='1986']").click(); // install 1986 y
+        $("select[class='react-datepicker__month-select']").$("option[value='10']").click(); // install nov
+        $x("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[2]").click(); // install 10
+         $(byText("Sports")).click(); // check sports in hobbies
+        $("#currentAddress").setValue("Adress 1");
 
-
+        $("#react-select-3-input").setValue("NCR");
 System.out.println("Простые текстовые поля были протестированы");
     }
     @Test
